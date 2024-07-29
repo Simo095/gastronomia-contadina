7b1d554cbb98f52e605537bc32ee5eae215bf786
Simone <135957682+Simo095@users.noreply.github.com>
2024-07-29 17:47:33 +0200

    add console.log

--- api/updateMenu.js
+++ api/updateMenu.js
@@ -9,34 +9,37 @@ export default async function updateMenuHandler(req, res) {
 
   try {
     const chunks = [];
-    req.on("data", chunk => chunks.push(chunk));
+    req.on("data", (chunk) => chunks.push(chunk));
     req.on("end", async () => {
       const buffer = Buffer.concat(chunks);
       const csvData = buffer.toString("utf-8");
       const parsedData = await parseCSV(csvData);
       const filename = "gc.json";
       const blob = await put(filename, JSON.stringify(parsedData), {
-        access: "public"
+        access: "public",
       });
-      return res.status(200).json({ message: "File updated successfully", url: blob.url });
+      return res
+        .status(200)
+        .json({ message: "File updated successfully", url: blob.url });
     });
   } catch (error) {
     return res.status(500).json({ error: error.message });
   }
 }
 
-const parseCSV = async csv => {
+const parseCSV = async (csv) => {
   const results = [];
   return new Promise((resolve, reject) => {
     const stream = Readable.from([csv]);
+    console.log("CSV =>", csv);
     stream
       .pipe(
         csvParser({
           headers: ["id", "type", "wardId", "wardName", "_5"],
-          separator: ";"
+          separator: ";",
         })
       )
-      .on("data", data => {
+      .on("data", (data) => {
         console.log("data=>", data);
         if (data.type && data.wardId && data.wardName && data._5) {
           const formattedPrice = data._5.replace(",", ".");
@@ -46,14 +49,14 @@ const parseCSV = async csv => {
             name: data.type,
             ward: {
               id: data.wardId,
-              name: data.wardName
+              name: data.wardName,
             },
             stock: 99999,
-            price: parseFloat(formattedPrice)
+            price: parseFloat(formattedPrice),
           });
         }
       })
       .on("end", () => resolve(results))
-      .on("error", error => reject(error));
+      .on("error", (error) => reject(error));
   });
 };
